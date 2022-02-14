05c24570e2eee02d465a155bab91f1c125917897
Tom Boyes-Park <tom.boyes-park@protonmail.com>
2022-02-14 18:37:12 +0000

    boilerplate app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+def main():
+    st.title("Rocket League Gameplay Statistics")
+
+    st.markdown(
+        "_Rocket League is a registered trademark of Psyonix. Trademarks are the "
+        "property of their respective owners. Game materials copyright Psyonix. "
+        "Psyonix has not endorsed and is not responsible for this site or its content._"
+    )
+
+
+if __name__ == "__main__":
+    main()
