332465bd1e61ac847b6c374f6a4fefd9fd368c22
Tom Boyes-Park <tom.boyes-park@protonmail.com>
2022-02-18 09:09:14 +0000

    simple README, change md to caption

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# rl-stats-app
+# Rocket League Statistics Web App 🚀
+
+## Getting Started
+
+Install requirements:
+
+```commandline
+pip install -r requirements.txt
+```
+
+Run the app:
+
+```commandline
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 def main():
     st.title("Rocket League Gameplay Statistics")
 
-    st.markdown(
+    st.caption(
         "_Rocket League is a registered trademark of Psyonix. Trademarks are the "
         "property of their respective owners. Game materials copyright Psyonix. "
         "Psyonix has not endorsed and is not responsible for this site or its content._"
